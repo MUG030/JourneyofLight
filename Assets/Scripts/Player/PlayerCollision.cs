38c5f1d4beec4d0a9103d0b8fc4631a063114227
MUG <ys8267928563@outlook.jp>
2023-10-05 23:06:13 +0900

    HP回復処理の完成

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -9,13 +9,7 @@ public class PlayerCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        hpBar = GetComponent<HPBar>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
