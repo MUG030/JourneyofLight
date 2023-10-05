48e0abc6370b1fb525717376b94178d8390051ba
MUG <ys8267928563@outlook.jp>
2023-10-05 12:06:52 +0900

    回復のinterfaceの作成

---
+++ Assets/Scripts/Managers/RecoveryItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoveryItem : MonoBehaviour, IRecoverable
+{
+    public int AddRecovery()
+    {
+        return 10;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public HPBar hpBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar = GetComponent<HPBar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var target = col.GetComponent<IRecoverable>();
+        if (target != null)
+        {
+            int RecoveryNum = target.AddRecovery();
+            hpBar.Recovery(RecoveryNum);
+            Debug.Log("体力を回復した");
+        }
+    }
+}
