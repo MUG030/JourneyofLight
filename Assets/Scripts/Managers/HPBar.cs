2f8f03c9ceba61f4726c465aba2e567724219c43
MUG <ys8267928563@outlook.jp>
2023-10-10 15:59:54 +0900

    サウンドマネージャーの自分のシーンへ反映

--- Assets/Scripts/Managers/HPBar.cs
+++ Assets/Scripts/Managers/HPBar.cs
@@ -10,6 +10,11 @@ public class HPBar : MonoBehaviour
     float currentHp;
     public Slider slider;
     private bool _isUmbrellaOpen = false; // 傘が開いているかどうかのフラグ
+    private bool _damageSprite1 = false;
+    private bool _damageSprite2 = false;
+
+    [SerializeField] private GameObject _playerObject;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
@@ -17,6 +22,11 @@ public class HPBar : MonoBehaviour
         currentHp = maxHp;
         // 5秒ごとにDamageメソッドを呼び出す
         InvokeRepeating("CallDamage", 0f, 1f);
+
+        if (_playerObject != null)
+        {
+            spriteRenderer = _playerObject.GetComponent<SpriteRenderer>();
+        }
     }
 
     private void CallDamage()
@@ -35,6 +45,23 @@ public class HPBar : MonoBehaviour
         {
             _isUmbrellaOpen = !_isUmbrellaOpen;
         }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Damage(5.0f);
+        }
+
+        if (_damageSprite1)
+        {
+            //spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+        else if (_damageSprite2)
+        {
+            //spriteRenderer.color = new Color(0.3f, 0.3f, 0.3f);
+        }
+        //else
+        //{
+        //    spriteRenderer.color = Color.white;
+        //}
     }
 
     public void Damage(float damage)
@@ -43,6 +70,22 @@ public class HPBar : MonoBehaviour
         if (currentHp <= 0)
         {
             currentHp = 0;
+        }
+
+        float switchHP = currentHp / maxHp;
+        if (switchHP >= 0.7f)
+        {
+            Debug.Log("7割超え");
+        }else if (switchHP >= 0.5f && switchHP < 0.7f)
+        {
+            Debug.Log("5割超え");
+            _damageSprite1 = true;
+            _damageSprite2 = false;
+        } else if (switchHP >= 0.3f && switchHP < 0.5f)
+        {
+            Debug.Log("3割超え");
+            _damageSprite1 = false;
+            _damageSprite2 = true;
         }
 
         slider.value = currentHp / (float)maxHp;
