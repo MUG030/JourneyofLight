12a030096cd9d150e89a659136ee1198b8fbcfeb
tattsu200 <141699900+tattsu200@users.noreply.github.com>
2023-10-20 23:17:02 +0900

    アメフラシ　玉（２）

---
+++ Assets/Scripts/Enemy/BulletDe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDe : MonoBehaviour
+{
+    // 削除する時間指定
+    public float deleteTime = 3.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 削除設定
+        Destroy(gameObject, deleteTime); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 何かに接触したら消す
+        Destroy(gameObject); 
+    }
+}
