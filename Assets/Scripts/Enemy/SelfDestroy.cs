04d9b2b11b093de08906043b90a9b5d4ce6ada7e
tattsu200 <141699900+tattsu200@users.noreply.github.com>
2023-10-28 13:09:14 +0900

    LostSpirit プレハブ化
    プレハブ化に伴い、一定以下の高度で消えるスクリプトを追加しました。

---
+++ Assets/Scripts/Enemy/SelfDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float destroyThresholdY = -10f; // 自身を削除するy軸の閾値
+
+    void Update()
+    {
+        // ゲームオブジェクトの現在の位置のy座標をチェック
+        if (transform.position.y <= destroyThresholdY)
+        {
+            // y軸が閾値以下になったら自身を削除
+            Destroy(gameObject);
+        }
+    }
+}
+
