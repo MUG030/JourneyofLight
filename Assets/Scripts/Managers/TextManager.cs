0a85e3e330ba7214d3d7d39dc5d55f54fc95b5fa
MUG <ys8267928563@outlook.jp>
2023-10-30 08:31:12 +0900

    エピローグシーンの作成
    Dotweenを用いたタイプライター風の作成

---
+++ Assets/Scripts/Managers/TextManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    public TypeWriteEffect typeWriteEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //typeWriteEffect.Show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Managers/TypeWriteEffect.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+
+public class TypeWriteEffect : MonoBehaviour
+{
+    public TMP_Text textMeshPro;
+    public List<string> textList;
+    public float typingSpeed = 0.1f;
+
+    private int currentIndex = 0;
+    private bool isTyping = false;
+
+    private void Start()
+    {
+        // 初期文章を表示
+        ShowNextText();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && !isTyping)
+        {
+            // Enterキーが押されたら次の文章を表示
+            ShowNextText();
+        }
+    }
+
+    private void ShowNextText()
+    {
+        if (currentIndex < textList.Count)
+        {
+            isTyping = true;
+            string textToType = textList[currentIndex];
+
+            // エスケープ文字 \n を実際の改行に変換
+            textToType = textToType.Replace("\\n", "\n");
+
+            // DOTweenを使ってテキストをクリアしてからタイプライター風に表示
+            textMeshPro.text = "";
+            DOTween.To(() => textMeshPro.text, x => textMeshPro.text = x, textToType, textToType.Length * typingSpeed)
+                .SetOptions(true)
+                .SetEase(Ease.Linear)
+                .SetUpdate(true)
+                .OnComplete(() =>
+                {
+                    isTyping = false;
+                    currentIndex++;
+
+                    if (currentIndex == textList.Count)
+                    {
+                        Debug.Log("全ての文章が表示されました。");
+                    }
+                });
+        }
+        else
+        {
+            Debug.Log("全ての文章が表示されました。");
+        }
+    }
+}
