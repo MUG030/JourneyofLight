22c72cf4f736ceed6986cd2d29bde286dc4ab5ec
MUG <ys8267928563@outlook.jp>
2023-11-14 15:33:05 +0900

    WorldLight2Dの編集
    敵を倒した時に明るくなる処理をリストで管理できる様にした。

--- Assets/Scripts/Light2D/WorldLight.cs
+++ Assets/Scripts/Light2D/WorldLight.cs
@@ -6,26 +6,25 @@ using UnityEngine.Rendering.Universal;
 public class WorldLight : MonoBehaviour
 {
     public Light2D light2D;
-    [SerializeField] private Color changeColor1;
-    [SerializeField] private Color changeColor2;
-    [SerializeField] private int _deadEnemy1;
-    [SerializeField] private int _deadEnemy2;
+    [SerializeField] private List<Color> changeColors;
+    [SerializeField] private List<int> deadEnemies;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(EnemyHP.deadEnemyCount == _deadEnemy1)
+        for (int i = 0; i < deadEnemies.Count; i++)
         {
-            light2D.color = changeColor1;
-        } else if (EnemyHP.deadEnemyCount == _deadEnemy2)
-        {
-            light2D.color = changeColor2;
+            if (EnemyHP.deadEnemyCount == deadEnemies[i])
+            {
+                light2D.color = changeColors[i];
+                break;
+            }
         }
     }
 }
