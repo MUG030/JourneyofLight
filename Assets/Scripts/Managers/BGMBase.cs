389e8897dc68addfc74125093bac90192113f49f
MUG <ys8267928563@outlook.jp>
2023-11-16 00:15:42 +0900

    サウンド設定

--- Assets/Scripts/Managers/BGMBase.cs
+++ Assets/Scripts/Managers/BGMBase.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BGMBase : MonoBehaviour
 {
     [SerializeField]
-    private SoundManager _soundManager;
-    [SerializeField]
     private AudioClip _clip;
 
     void Start()
     {
+        // SoundManagerをFindで検索して取得
+        SoundManager _soundManager = GameObject.FindObjectOfType<SoundManager>();
+
         if (_clip != null)
         {
             Debug.Log("音を鳴らす");
